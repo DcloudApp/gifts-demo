4eed2769d4d27e486d21281b6485e4818f87d78f
cc <cc@example.com>
2026-05-07 23:22:35 +0800

    ✨ feat:xg

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ import Layouts from 'vite-plugin-vue-layouts'
 const timesTamp = new Date().getTime()
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
